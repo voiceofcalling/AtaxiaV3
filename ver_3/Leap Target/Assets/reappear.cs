6e64c7356876bb8e607270842e9bb2ef2336a173
Chuyeon Lee <wonup2@hotmail.com>
2022-09-05 20:09:33 -0700

    AtaxiaV3.2

---
+++ ver_3/Leap Target/Assets/reappear.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reappear : MonoBehaviour
+{
+    Vector3 originalPosthr;
+    Vector3 originalPostarget;
+    GameObject thr;
+    GameObject target;
+    Rigidbody thrrigid;
+    Rigidbody targetrigid;
+    // -1.1<=x<=1.1  0.1<=y<=1.1 z = 2.7
+    // Start is called before the first frame update
+    void Start()
+    {
+        thr = GameObject.Find("Throw");
+        target = GameObject.Find("Target");
+        originalPosthr = thr.transform.position;
+        originalPostarget = target.transform.position;
+        thrrigid = (Rigidbody)thr.GetComponent("Rigidbody");
+        targetrigid = (Rigidbody)target.GetComponent("Rigidbody");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            reset();
+        }
+    }
+    public void reset()
+    {
+        thr.transform.position = originalPosthr;
+        float x = Random.Range(-1.1f, 1.1f);
+        float y = Random.Range(0.1f, 1.1f);
+        target.transform.position = new Vector3(x,y,2.5f);
+        thr.transform.rotation = Quaternion.identity;
+        target.transform.rotation = Quaternion.identity;
+        thrrigid.velocity = new Vector3(0, 0, 0);
+        targetrigid.velocity = new Vector3(0, 0, 0);
+        thrrigid.angularVelocity = new Vector3(0, 0, 0);
+        targetrigid.angularVelocity = new Vector3(0, 0, 0);
+    }
+}
---
+++ ver_3/Leap Touch Rhythm/Assets/Plugins/Reappear.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reappear : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody rb;
+    int timer;
+    void Start()
+    {
+        rb = (Rigidbody)gameObject.GetComponent("Rigidbody");
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer++;
+        if (timer == 50)
+            timer = 0;
+        if(gameObject.transform.position.z!=0.5|| rb.velocity.x!=0||rb.velocity.y!=0||rb.velocity.z!=0)
+        {
+            Reset();
+        }
+    }
+    public void Reset()
+    {
+        if (timer != 0)
+            return;
+        float X = Random.RandomRange(-0.6f, 0.4f);
+        float Y = Random.RandomRange(-.1f,0.2f);
+        gameObject.transform.position = new Vector3(X, Y, 0.5f);
+        gameObject.transform.rotation = Quaternion.identity;
+        rb.velocity = new Vector3(0, 0, 0);
+        rb.angularVelocity = new Vector3(0, 0, 0);
+
+    }
+}
